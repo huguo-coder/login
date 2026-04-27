56465bfde5c3b3aaf06dd2920c3e2955fbd91c66
huguo-coder <hu.guo@iisgalvanimi.edu.it>
2026-04-27 10:48:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import pandas as pd
+
 
 app = FastAPI()
-df=pd.read_exel()
+df=pd.read_excel("huguo-coder/login/Foglio di lavoro senza nome.xlsx")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/") # endpoint  punto dove chimiamo il nostro server web, lo / e la homepage
 def home():
@@ -14,11 +17,20 @@ def home():
 def Controlla(username: str = Form(...), password: str = Form(...)):
     print("username", username, "password", password)
 
+
     if username == "admin" and password == "xxx123":
         risposta = {"messaggio": 1}
     else:
         risposta = {"messaggio": 0}
 
+
     return risposta
  
+@app.post("/loginpandas")
+def Controlla_password(username: str = Form(...), password: str = Form(...)):
+risultato=df[(df["username"==username]])&(df["password"==password])]
+if not risultato.empty:
+    return {"messaggio":1}
+    else:
+        return{"messaggio":0}
 
